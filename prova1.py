eada8849f3b9f90808cf0076c763e0fe6531e442
igomez21206-blip <igomez21206@iesjoanramis.org>
2025-11-12 13:30:01 +0100

    1

---
+++ prova1.py
@@ -0,0 +1,7 @@
+x = int(input())
+y = int(input())
+z = int(input())
+a = x + y + z
+print("El resultat de {} + {} + {} és {}".format(x, y, z, a))
+a = x * y * z
+print("El resultat de {} * {} * {} és {}".format(x, y, z, a))
