da6934474937a530d23d05b0c17daccbfd157b21
igomez21206-blip <igomez21206@iesjoanramis.org>
2025-11-12 13:56:45 +0100

    ex2

--- prova1.py
+++ prova1.py
@@ -5,3 +5,14 @@ a = x + y + z
 print("El resultat de {} + {} + {} és {}".format(x, y, z, a))
 a = x * y * z
 print("El resultat de {} * {} * {} és {}".format(x, y, z, a))
+if c<20:
+    print("la suma és major que 20, que és {}".format(c)
+          else:
+    print("la suma es menor que 20, que és {}".format(c))
+c = x + y + z
+print("El resultat de la multiplicacio {} * {} * {} és {}".format(a, b, c))
+if c>100
+print("la multiplicació és major que 100, que és {}".format(c))
+else
+    print("la multiplicació es menor que 100, que és {}".format(c))
+    
