4bc32584cbbbaa0dbc35f72aa3a9bac1d3ad12ed
igomez21206-blip <igomez21206@iesjoanramis.org>
2025-11-13 11:46:24 +0100

    Ex12.py

---
+++ EX12.py
@@ -0,0 +1,10 @@
+edat = int(input("Introdueix la teva edat: "))
+       
+if edat > 18:
+    print("Ets major d'edat.")
+elif edat < 18:
+    print("Ets menor d'edat.")
+else:
+    print("te exactament 18 anys.")
+      
+    
